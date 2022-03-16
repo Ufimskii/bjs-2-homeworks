2e015a7d5222779b5352fbc3423abea9de76c24b
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-03-16 21:55:34 +0500

    add task_js_HW

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
 function solveEquation(a, b, c) {
+  if(a == 0)
+      return false;
   let arr;
-  // код для задачи №1 писать здесь
+  let D = b * b - 4 * a * c;
+  console.log("D = " + D);
+  if(D < 0)
+      return false;
+  arr = D;
+  if(D == 0)
+      arr = (-b + Math.sqrt(D)) / (2 * a);
+  else if(D > 0){
+      let tmp = [];
+      tmp.push((-b + Math.sqrt(D)) / (2 * a));
+      tmp.push((-b - Math.sqrt(D)) / (2 * a));
+      arr = tmp;
+  }
   return arr; // array
 }
 
