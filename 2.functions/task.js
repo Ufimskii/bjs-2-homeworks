5fcb2b68c3c302e2b33a7b4bdd05ea9fce87c45e
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-03-18 21:42:58 +0500

    func_add_tasks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,48 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+  for (i = 0; i < arr.length; i += 1) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (i = 0; i < arr.length; i += 1) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (i = 0; i < arrOfArr.length; i += 1) {
+    arrResultElement = func(arrOfArr[i]);
+    if (arrResultElement > max) max = arrResultElement;
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  for (i = 0; i < arr.length; i += 1) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+  }
+  let difference = max - min;
+  return difference;
 }
