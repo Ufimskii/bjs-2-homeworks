170c0e2b5c87e198f90342665985d03d6f153b61
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-03-19 22:23:36 +0500

    base_concept_hw_v_3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,16 @@
 function solveEquation(a, b, c) {
-  if(a == 0)
-      return [];
-  let arr=[];
+  if (a == 0)
+    return [];
+  let arr = [];
   let D = b * b - 4 * a * c;
   console.log("D = " + D);
-  if(D < 0)
-      return [];
-  if(D == 0)
-      arr = ((-b ) / (2 * a));
-  else if(D > 0){
-      arr.push((-b + Math.sqrt(D)) / (2 * a));
-      arr.push((-b - Math.sqrt(D)) / (2 * a));
-      arr = tmp;
+  if (D < 0)
+    return [];
+  if (D == 0)
+    arr.push((-b) / (2 * a));
+  else if (D > 0) {
+    arr.push((-b + Math.sqrt(D)) / (2 * a));
+    arr.push((-b - Math.sqrt(D)) / (2 * a));
   }
   return arr; // array
 }
