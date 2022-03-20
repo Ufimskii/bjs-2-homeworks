179158c019a9de6ddbdbbcf1a3301fc69e143f5f
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-03-20 20:54:11 +0500

    arr_task_add

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = false;
+  if (arr1.length !== arr2.length)
+    return result;
+  if (arr1.every((n, i) => n === arr2[i]))
+    result = true;
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr
+    .filter((element) => element > 0)
+    .filter((element) => element % 3 === 0)
+    .map((element) => element * 10);
   return resultArr; // array
 }
