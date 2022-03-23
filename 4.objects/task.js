739e1d3514f80cff7393b8277081083a70919fb1
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-03-23 21:01:12 +0500

    obj_task_add

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
+"use strict";
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+};
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    let marks = [];
+    this.marks = marks;
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  };
+};
+
+Student.prototype.addMarks = function (...rest) {
+  if (this.marks === undefined) {
+    let marks = [];
+    this.marks = marks;
+    this.marks.push(...rest);
+  } else {
+    this.marks.push(...rest);
+  };
+};
+
+Student.prototype.getAverage = function () {
+  const average = this.marks.reduce((acc, item, idx, arr) => {
+    acc += item;
+    if (idx === arr.length - 1) {
+      return acc / arr.length;
+    } else return acc;
+  })
+
+  return average;
 }
 
-// ваш код для остальных методов
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
